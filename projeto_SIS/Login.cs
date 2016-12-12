1b69f64b424079035c70dfb4e49d55160bbd95ce
David Santos <davidfmsantos@gmail.com>
2016-12-12 22:16:42 +0000

    Alterações no login
    Try catch no login

--- projeto_SIS/Login.cs
+++ projeto_SIS/Login.cs
@@ -24,42 +24,52 @@ namespace projeto_SIS
         {   
             if (textBoxUsername.TextLength > 0 && textBoxPassword.TextLength > 0)
             {
-                if (textBoxUsername.Text == "admin")
+                try
                 {
-                    RestClient client = new RestClient("http://localhost/projeto_platsi/api/web/v1/user/authenticate");
-                    RestRequest request = new RestRequest();
-
-                    request.Method = Method.POST;
-                    request.AddHeader("Accept", "application/json");
-                    request.AddHeader("Content-Type", "application/json");
-                    
-                    request.AddJsonBody(
-                     new
-                     {
-                         username = textBoxUsername.Text,
-                         password = textBoxPassword.Text
-                     }
-                    );
-
-                    IRestResponse response = client.Execute(request);
-
-                    Utils.escreverParaFicheiro("Pedido", client.BaseUrl.ToString(), request.Method.ToString(), request.Parameters);
-
-                    string content = response.Content;
-                    string access_token = JObject.Parse(content).Property("access_token").Value.ToString();
-                
-                    if (access_token != null)
+                    if (textBoxUsername.Text == "admin")
                     {
-                        this.Hide();
-                        Dashboard dashboard = new Dashboard(access_token);
-                        dashboard.ShowDialog();
-                        this.ShowDialog();
+                        RestClient client = new RestClient("http://localhost/projeto_platsi/api/web/v1/user/authenticate");
+                        RestRequest request = new RestRequest();
+
+                        request.Method = Method.POST;
+                        request.AddHeader("Accept", "application/json");
+                        request.AddHeader("Content-Type", "application/json");
+
+                        request.AddJsonBody(
+                         new
+                         {
+                             username = textBoxUsername.Text,
+                             password = textBoxPassword.Text
+                         }
+                        );
+
+                        IRestResponse response = client.Execute(request);
+
+                        Utils.escreverParaFicheiro("Pedido", client.BaseUrl.ToString(), request.Method.ToString(), request.Parameters);
+
+                        string content = response.Content;
+                        string access_token = JObject.Parse(content).Property("access_token").Value.ToString();
+
+                        if (access_token != null)
+                        {
+                            this.Hide();
+                            Dashboard dashboard = new Dashboard(access_token);
+                            dashboard.ShowDialog();
+                            Application.Exit();
+                                               
+                        }
+                        else
+                            MessageBox.Show("Algo de errado aconteceu.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                     else
-                        MessageBox.Show("Algo de errado aconteceu.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);                    
+                        MessageBox.Show("Utilizador sem permissões para fazer autenticação.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                else
-                    MessageBox.Show("Utilizador sem permissões para fazer autenticação.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);                  
+                catch (Exception)
+                {
+                    MessageBox.Show("Algo inesperado aconteceu", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                                  
             }
             else
                 MessageBox.Show("Os campos não foram preenchidos.", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
